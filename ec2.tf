63b3d6ac455b3c11dfda7772134f18a173d3646f
Akomolafegrace <Akomolafegrace06@gmail.com>
2025-10-16 23:18:06 -0400

    new parameter file modified

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,13 @@
 resource "aws_instance" "server1" {
-  instance_type          = "t2.micro"
+  instance_type          = var.INSTANCE_TYPE
   user_data              = file("setup.sh")
   vpc_security_group_ids = [aws_security_group.sg-demo1.id]
   subnet_id              = aws_subnet.private1.id
-  availability_zone      = "us-east-1a"
+  availability_zone      = "${var.REGION}a"
   ami                    = "ami-0150ccaf51ab55a51"
   tags = {
     name = "web1"
+    name = var.ENVIRONMENT
   }
 }
 #second ec2
