9828219be67c8eb949e087104d56740f59344f1a
Akomolafegrace <Akomolafegrace06@gmail.com>
2025-10-31 19:39:56 -0400

    jenkinsfile

--- variable.tf
+++ variable.tf
@@ -1,11 +1,14 @@
 variable "ENVIRONMENT" {
-  
+  description = "AWS region to deploy resources"
+  default = "us-east-1"
 }
 
 variable "REGION" {
-  
+  description = "Deployment environment"
+  default = "dev"
 }
 
 variable "INSTANCE_TYPE" {
-  
+  description = "Ec2 instance type"
+  default = "t2.micro"
 }
